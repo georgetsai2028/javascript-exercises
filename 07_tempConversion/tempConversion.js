aff595b3bb58e32910aa95a8b1aa4914bb1250bc
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2024-12-16 14:50:51 -0800

    finished exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius = 0;
+  celcius = (fahrenheit - 32) * .5556;
+  return parseFloat(celcius.toPrecision(3));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = 0;
+  fahrenheit = (celcius * 1.8 + 32);
+  return parseFloat(fahrenheit.toPrecision(4));
 };
 
 // Do not edit below this line
