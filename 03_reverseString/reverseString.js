078d08fcee36431c7edd4893cd4c8cf328a35ca8
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2024-12-15 19:02:54 -0800

    exercise 4 finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(word)
+{
+    let result = "";
+    for (let i = word.length - 1; i >= 0; i--)
+    {
+        if (word[i] = "")
+        {
+            result += "";
+        }
+        result += word[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
