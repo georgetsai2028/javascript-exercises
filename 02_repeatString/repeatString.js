e7d508a13653478bcb8f39cb02393d22cfdd45f0
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2024-12-15 18:22:34 -0800

    finished second excersize

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+number = Math.floor(Math.random() * 1000)
 
+const repeatString = function(word, number) {
+    let result = '';
+    if (number < 0)
+    {
+        result = "ERROR";
+    }
+    for (let i = 0; i < number; i++)
+    {
+        result += word;
+    }
+    return result;
 };
 
 // Do not edit below this line
