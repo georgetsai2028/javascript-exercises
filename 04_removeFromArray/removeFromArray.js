459a87cb66e357f310f8b458e7818d93b04fcc73
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2024-12-16 10:59:31 -0800

    exercise 4 completed previous was 3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...remove) {
+    return arr.filter(item => !remove.includes(item));
+
 };
 
 // Do not edit below this line
