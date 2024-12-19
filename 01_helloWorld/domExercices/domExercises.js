4f95f5f4a958ab765b673f1a7fac5cdef31d64fe
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2024-12-19 13:43:13 -0800

    learning doms

--- 01_helloWorld/domExercices/domExercises.js
+++ 01_helloWorld/domExercices/domExercises.js
@@ -1,3 +1,36 @@
-const redText = document.createElement(redText);
-redText.innerHTML = <p>Hey I'm red!</p>;
-redText.setAttribute("style", "color: red;");
+
+const container = document.createElement("div");
+container.style.border = "1px solid black";
+container.style.backgroundColor = "pink";
+
+const inDiv = document.createElement("h1");
+inDiv.textContent = "I'm in a div";
+container.appendChild(inDiv);
+
+const secondInDIV = document.createElement("p");
+secondInDIV.textContent = "ME TOO!";
+container.appendChild(secondInDIV);
+
+const redText = document.createElement("p");
+redText.textContent = "Hey I'm red!";
+redText.style.color = "red";
+document.body.appendChild(redText);
+
+console.log(redText);
+
+const blueText = document.createElement("h3");
+blueText.textContent = "I'm a blue h3!";
+blueText.style.color = "blue";
+document.body.appendChild(blueText);
+
+document.body.appendChild(container);
+
+function alertFunction() {
+    alert("IT WORKS!");
+}
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", function (e) {
+    e.target.style.background = "blue";
+    console.log(e.target);
+});
