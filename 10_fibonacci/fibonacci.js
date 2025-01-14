530762601f4445a2c0245fc1033cb7c0cdd701dd
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-01-13 17:42:30 -0800

    fibonacchi task and testing completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibNumber) {
+let firstNum = 0;   
+let secondNum = 1;
+let nextNum;
+if (fibNumber < 0)
+{
+    return "OOPS";
+}
+if (fibNumber == 0)
+{
+    return firstNum;
+}
+if (fibNumber == 1)
+{
+    return secondNum;
+}
+for(let i = 2; i <= fibNumber; i++)
+{
+    nextNum = firstNum + secondNum;
+    firstNum = secondNum;
+    secondNum = nextNum;
+}
+return nextNum;
 };
 
 // Do not edit below this line
